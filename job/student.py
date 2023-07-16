5b93b10428b612cfd38650e06f692ade8bd40ce0
Arkashka <sevenkapsul@mail.ru>
2023-07-16 11:28:30 +0300

    finish home work 12

--- job/student.py
+++ job/student.py
@@ -1,7 +1,28 @@
+from lesson import Lesson
+from descriptor import Descriptor_name
+
+
 class Student:
-	
-	def __init__(self, name, last_name, ogli):
-		self.name = name
-		self.last_name = last_name
-		self.ogli = ogli
-		
+    """
+    Класс студента хранящий в себе его имя, фамилию, отчество
+    и список его предметов
+    """
+    first_name = Descriptor_name()
+    last_name = Descriptor_name()
+    patronymic = Descriptor_name()
+
+    def __init__(self, first_name, last_name, patronymic):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.patronymic = patronymic
+        self.list_lesson = []
+
+    def append_lesson(self, lesson: Lesson):
+        self.list_lesson.append(lesson)
+
+    def print_list_lesson(self):
+        for lesson in self.list_lesson:
+            print(lesson)
+
+    def __str__(self):
+        print(f'{self.first_name = },{self.last_name = },{self.patronymic = }\n{self.list_lesson = }')
